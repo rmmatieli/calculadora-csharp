4619924a2f34a193c25b1ce8dc39ad5ff2af4f48
Ramom Matieli <ramommatieli@hotmail.com>
2022-08-30 17:40:23 -0300

    Add files via upload

---
+++ Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Soma();
+        }
+
+
+        static void Soma() {
+            Console.Clear();
+            //Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 + v2;
+
+            Console.WriteLine("Soma: {resultado}");
+            // Console.WriteLine("Soma: {v1 + v2}");
+            // Console.WriteLine("Soma: " + resultado);
+        }
+    }
+}
